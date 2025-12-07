c0873abf24556a66dac78ba3028f56439ac7ecab
mkou1256 <zenokoki3@gmail.com>
2025-12-08 00:36:10 +0900

    FreeRTOSのお試し - 二つのタスクからdbgPrint - 文字列が混ざるので排他が必要 - 送受信を非同期に行う機構も作りたい

--- src/system/system_entry.c
+++ src/system/system_entry.c
@@ -3,6 +3,9 @@
 #include "typedef.h"
 #include "dbg_print.h"
 #include "usb_comm.h"
+#include "task_test.h" // test
+#include "FreeRTOS.h"
+#include "task.h"
 
 int8_t systemInit()
 {
@@ -31,6 +34,35 @@ int main() {
     if (systemInit() != E_SUCCESS) {
         return -1;
     }
+
+    BaseType_t ret = xTaskCreate(
+        task1,    // タスク関数
+        "Task1",          // タスク名
+        256,              // スタックサイズ
+        NULL,             // パラメータ
+        1,                // 優先度
+        NULL              // タスクハンドル
+    );
+    if (ret != pdPASS) {
+        dbgPrint(DBG_LEVEL_ERROR, "Failed to create Task1\r\n");
+        return -1;
+    }
+
+    ret = xTaskCreate(
+        task2,    // タスク関数
+        "Task2",          // タスク名
+        256,              // スタックサイズ
+        NULL,             // パラメータ
+        1,                // 優先度
+        NULL              // タスクハンドル
+    );
+    if (ret != pdPASS) {
+        dbgPrint(DBG_LEVEL_ERROR, "Failed to create Task2\r\n");
+        return -1;
+    }
+
+    dbgPrint(DBG_LEVEL_INFO, "Starting scheduler...\r\n");
+    vTaskStartScheduler();
 
     while (true) {
         cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);  // LED ON
